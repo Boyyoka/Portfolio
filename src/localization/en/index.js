a28ba73829a74e683b9302de283bd0ef522563c1
Boyyoka <boykehooplot@hotmail.com>
2019-05-11 21:11:57 +0900

    Updated constants. Finished work section

--- src/localization/en/index.js
+++ src/localization/en/index.js
@@ -1,4 +1,23 @@
 export default {
+  //General,
+  FEBRUARY: `February`,
+  SEPTEMBER: `September`,
+  APRIL: `April`,
+  MARCH: `March`,
+  MAY:  `May`,
+  JUNE: `June`, 
+  JULY: `July`,
+  AUGUST:  `August`,
+  DECEMBER: `December`,
+  TWO_THOUSAND_EIGHTEEN: `2018`,
+  TWO_THOUSAND_THIRTEEN: `2013`,
+  TWO_THOHUSAND_SEVENTEEN: `2017`,
+  PRESENT: `Present`,
+  FULL_STACK_ENGINEER: `Fullstack software engineer`,
+  INTERN: `Intern`,
+  PART_TIME_DEVELOPER: 'Part-time developer',    
+
+
   //Introduction
   MY_NAME: 'Shensly Hooplot',
   GENERAL_INTRODUCTION: `I'm Shensly Hooplot`,
@@ -35,5 +54,38 @@ export default {
   I'm most familiar with C# and the .Net framework, but picking a new framework or language is no problemo.
   likewise, I love building things from scratch putting in a lot of thought before hand and making proper software designs to address future problems.`,
 
-  CONTACT_DETAILS_TITLE: ``
+  CONTACT_DETAILS_TITLE: ``,
+
+  //Resume
+  EDUCATION: `Education`,
+  THE_HAGUE_UNIVERSITY: `The Hague University Of Applied Sciences`,
+  NOVA_COLLEGE: `Nova College`,
+  BACHELOR_DEGREE: `Bachelor of Information Technology`,
+  ASSOCIATES_DEGREE: `Associates of Application Developer (Multimedia&Design)`,
+
+  BACHELOR_DESCRIPTION: `I studied Information Technology (now reformed to Computer Science) at The Hague University Of Applied Sciences. Here I gained a strong knowledge 
+  in a wide variety of subjects including software development, software security, software design, data structures, game development, database design & deployment, software testing,
+  requirements gathering & specifications and project management.        
+  `,
+  ASSOCIATES_DESCRIPTION: `I completed this program at Nova College. The first year of this program consisted out of hardware, networking and software courses. This combination
+  gave me a good understanding of both hardware and software. The whole program was originally a three year program, but I graduated a year early.
+  `,
+
+  WORK: `Work`,
+  BLOCK_BUSKER: `BlockBusker`,
+  BIT_HOLLA: `BitHolla`,
+  BLUE_SPHERE: `BlueSphere`,
+  AUTODEALERS: `Autodealers`,
+  X_S_4_All: ``,
+
+  BLOCK_BUSKER_DESCRIPTION: `As a full-stack developer (javascript), I'm responsible for implementing new functionality into the current platform, maintaining the code and making sure that everything is well tested
+  and good to go for launch. The platform called `,
+  BLOCK_BUSKER_DESCRIPTION_2: `is a language based learning tool for people who want to learn language a new language in a fun and interesting way.`,
+  BIT_HOLLA_DESCRIPTION: `I have held two different positions within this company. From full time intern to full time employee as a fullstack engineer (javascript) in a span of four months. During my time here, I spent time building
+  the API in the backend, implementing deposit and withdrawal functionality and implementing hotkeys and macros (keyboard shortcuts to execute custom macro functionality) for the platform called:
+  `,
+  BIT_HOLLA_DESCRIPTION_2: `which is a crypto market that makes trading your cryptocurrencies intiuitive and rapid.`,
+  BLUE_SPHERE_DESCRIPTION:  `As a part-time developer I was working on a project that involved developing an Android application. My tasks mostly involved implementing new features and fixing issues that plagued the application at that time. `,
+  AUTODEALERS_DESCRIPTION:  `The position I held within this company was an intern. Most of my work involved building websites (front-end) with HTML, CSS and Javascript (JQuery). I also had to create my own application for the company which I did with HTML, CSS and C#.net`
+  
 }
